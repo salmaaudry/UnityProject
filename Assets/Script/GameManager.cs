76d04b4cc05b001a557b8721546de952910d40a9
Salma Tri Audryani <salmaaudry@gmail.com>
2022-04-13 00:02:37 +0700

    Unity Project

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject PanelPause;
+    public void PauseControl()
+    {
+        if (Time.timeScale == 1){
+            PanelPause.SetActive (true);
+            Time.timeScale = 0;
+        }else {
+            Time.timeScale = 1;
+            PanelPause.SetActive (false);
+        }
+    }
+
+    public void MenuUtama (string sceneName){
+        SceneManager.LoadScene(sceneName);
+    }
+}
